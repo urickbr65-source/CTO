9d1e079371ca6e0b06e2b829aeacc27139b8bcfd
Rickznx123 <ricksantos1428@gmail.com>
2026-07-23 12:31:39 -0400

    feat: adiciona suporte ao Google Ads

--- src/components/analytics/AnalyticsProvider.tsx
+++ src/components/analytics/AnalyticsProvider.tsx
@@ -5,9 +5,11 @@ import { usePathname } from "next/navigation";
 import Script from "next/script";
 import {
   GA_MEASUREMENT_ID,
+  GOOGLE_ADS_ID,
   GTM_ID,
   META_PIXEL_ID,
   hasValidGaId,
+  hasValidGoogleAdsId,
   hasValidGtmId,
   hasValidMetaPixelId,
 } from "@/lib/analytics/env";
@@ -38,13 +40,16 @@ export function AnalyticsProvider() {
 
       const doc = document.documentElement;
       const scrollable = doc.scrollHeight - doc.clientHeight;
+
       if (scrollable <= 0) {
         return;
       }
 
       const progress = (window.scrollY / scrollable) * 100;
+
       if (progress >= 90) {
         hasTrackedScroll.current = true;
+
         trackEvent("scroll_90", {
           path: `${window.location.pathname}${window.location.search}`,
         });
@@ -52,6 +57,7 @@ export function AnalyticsProvider() {
     };
 
     window.addEventListener("scroll", onScroll, { passive: true });
+
     return () => window.removeEventListener("scroll", onScroll);
   }, []);
 
@@ -66,6 +72,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','${GTM_ID}');`}
           </Script>
+
           <noscript>
             <iframe
               src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
@@ -83,15 +90,28 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
             strategy="afterInteractive"
           />
+
           <Script id="ga4-base" strategy="afterInteractive">
-            {`window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);} 
+            {`
+window.dataLayer = window.dataLayer || [];
+
+function gtag(){dataLayer.push(arguments);}
+
 window.gtag = gtag;
+
 gtag('js', new Date());
+
 gtag('config', '${GA_MEASUREMENT_ID}', {
   send_page_view: false,
   anonymize_ip: true
-});`}
+});
+
+${
+  hasValidGoogleAdsId
+    ? `gtag('config', '${GOOGLE_ADS_ID}');`
+    : ""
+}
+`}
           </Script>
         </>
       ) : null}
@@ -105,13 +125,15 @@ n.callMethod.apply(n,arguments):n.queue.push(arguments)};
 if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
 n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
+s.parentNode.insertBefore(t,s)}
+(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
+
 fbq('init', '${META_PIXEL_ID}');
 fbq('track', 'PageView');`}
           </Script>
+
           <noscript>
-            {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
               height="1"
               width="1"
